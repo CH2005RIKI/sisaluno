487f1d075e7e67ca64879bfda990f893c07f1a46
Carlos Henrique da Rocha Porto <129870078+CH2005RIKI@users.noreply.github.com>
2023-08-08 23:30:02 -0300

    Update cruddisciplina.php

--- disciplina/cruddisciplina.php
+++ disciplina/cruddisciplina.php
@@ -15,8 +15,22 @@ function validarNome($nome) {
     return preg_match('/^[a-zA-ZÀ-ÖØ-öø-ÿ\s]+$/', $nome);
 }
 
-##cadastrar
+## Fetch and display professor IDs
+$query = "SELECT id FROM professor";
+$result = $conexao->query($query);
+if ($result) {
+    /*echo "<strong>Professores disponiveis IDs:</strong><br>";
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        echo $row['id'] . "<br>";
+    }
+    echo "<br>";
+} else {
+    echo "Error fetching professor IDs: " . $conexao->errorInfo()[2];*/
+}
+
+## Cadastrar
 if (isset($_POST['cadastrar'])) {
+    $professorId = $_POST['professorid']; // Assuming you have an input for professor ID
     $nome = $_POST['nomedisciplina'];
     $semestre = $_POST['semestre'];
     $ch = $_POST['cargahoraria'];
@@ -27,18 +41,18 @@ if (isset($_POST['cadastrar'])) {
     }
 
     ## Using parameterized queries to prevent SQL injection
-    $sql = "INSERT INTO disciplina (nome, ch, semestre) VALUES (:nome, :ch, :semestre)";
+    $sql = "INSERT INTO disciplina (nome, ch, semestre, idprofessor) VALUES (:nome, :ch, :semestre, :professorId)";
     $stmt = $conexao->prepare($sql);
     $stmt->bindParam(':nome', $nome, PDO::PARAM_STR);
     $stmt->bindParam(':ch', $ch, PDO::PARAM_STR);
     $stmt->bindParam(':semestre', $semestre, PDO::PARAM_STR);
+    $stmt->bindParam(':professorId', $professorId, PDO::PARAM_INT);
 
     if ($stmt->execute()) {
         echo "<strong>OK!</strong> A Disciplina $nome foi incluída com sucesso!";
         echo "<button class='button'><a href='./listadisciplina.php'>lista</a></button>";
     }
 }
-
 #######alterar
 if (isset($_POST['update'])) {
     $id = $_POST['id'];
